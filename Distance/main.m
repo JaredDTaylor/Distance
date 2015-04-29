1c0fd392bd342bcbf91d3867129d752cca275cef
Jared <jaredtaylor@cbrc.net>
2015-04-29 09:46:07 -0400

    function and tests

--- Distance/main.m
+++ Distance/main.m
@@ -7,11 +7,41 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <math.h>
+double distance(double x1, double y1, double x2, double y2){
+    double x3 = x2-x1;
+    double y3 = y2-y1;
+    double x4 = pow(x3,2);
+    double y4 = pow(y3,2);
+    double sum = x4+y4;
+    return sqrt(sum);
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if (distance(0,0,0,1) != 1){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,0,0) != 0){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,1,0) != 1){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,1,1)-1.41>0.05){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,3,3)-4.24>0.05){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,10,10)-14.14>0.05){
+            NSLog(@"Incorrect");
+        }
+        if (distance(0,0,15,15)-21.21>0.05){
+            NSLog(@"Incorrect");
+        }
+        
+        
     }
     return 0;
 }
